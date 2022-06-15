79eadd9ae6223b5e59db3d786917c271748f6812
carlalwhite <c.l.white@swansea.ac.uk>
2022-06-15 12:56:46 +0100

    Update single scenario script

--- inst/scripts/ringbp_single.R
+++ inst/scripts/ringbp_single.R
@@ -2,64 +2,46 @@ library(ringbp)
 library(dplyr)
 library(ggplot2)
 
-# res <- ringbp::scenario_sim(n.sim = 100, num.initial.cases = 1,prop.asym=0,
-#                             prop.ascertain = 0, cap_cases = 4500, cap_max_days = 90,
-#                             r0isolated = 1.2, r0community = 2.1, disp.com = 0.16, disp.iso = 2, delay_shape = 1.651524,
-#                             delay_scale = 4.287786,k = 0, quarantine = FALSE)
+params = list(n.sim = 100,
+           num.initial.cases = 10,
+           prop.asym=0,
+           prop.ascertain = 0,
+           cap_cases = 4500,
+           cap_max_days = 120,
+           r0isolated = 0,
+           r0community = 0.8,
+           disp.com = 0.5,
+           disp.iso = 0.6,
+           dist_shape = 3,
+           dist_scale = 11,
+           delay_shape = 1.1,
+           delay_scale = 4,
+           k = 4)
 
-results <- scenario_sim(n.sim = 500,
-                        num.initial.cases = 1,
-                        prop.asym=0,
-                        prop.ascertain = 0,
-                        cap_cases = 4500,
-                        cap_max_days = 90,
-                        r0isolated = 1.3,
-                        r0community = 2.5,
-                        disp.com = 0.38,
-                        disp.iso = 1,
-                        dist_shape = 2.694934,
-                        dist_scale = 11.24573,
-                        delay_shape = 0.999976,
-                        delay_scale = 1.99998,
-                        k = 0)
+results <- do.call(scenario_sim, params)
 
-# Plot of daily cases
-gg <- results %>%
-  ggplot()+
-  geom_line(aes(x=day, y=cumulative, col = as.factor(sim)),alpha=0.6,show.legend = FALSE)+
-  scale_y_continuous(name="Number of cases")
+daily_results <- map(results, create_case_data, params$n.sim, params$cap_max_days) %>%
+  data.table::rbindlist() %>%
+  mutate(sim = rep(1:params$n.sim, rep(floor(params$cap_max_days) + 1, params$n.sim)))
 
-outbreak_sims <- results %>%
-  filter(day==90 & cumulative > 10) %>%
-  select(sim)
+avg_data <- create_averages_data(daily_results)
+avg_data[avg_data$day==120,]
 
-outbreak_results <- results %>%
-  filter(sim %in% outbreak_sims$sim)
+plot_data <- sims_to_plot(daily_results, no_of_sims=10, end_day=params$cap_max_days)
 
-outbreak_mean <- outbreak_results %>%
-  group_by(day) %>%
-  summarise(mean=mean(cumulative),median=median(cumulative))
+ggplot2::ggplot() +
+  ggplot2::geom_line(data=plot_data, ggplot2::aes(x=day, y=cumulative, group = as.factor(sim)),
+                     colour= "#D55E00", alpha = 0.3) +
+  ggplot2::geom_line(data=avg_data, ggplot2::aes(x=day, y=value, group = label, colour = label)) +
+  ggplot2::scale_color_manual(values = c("median"="#0072B2","mean"="#009E73")) +
+  ggplot2::scale_y_continuous(name="Number of cases") +
+  ggplot2::theme_bw() +
+  theme(legend.title=element_blank(),legend.position = c(0.1, 0.9))
 
-gg <- gg+
-  geom_line(data=outbreak_mean,aes(x=day,y=mean),colour="black")+
-  geom_line(data=outbreak_mean,aes(x=day,y=median),colour="blue")
 
-# data <- read.csv("data/monkeypox_uk.csv")
-#
-# gg <- gg+
-#   geom_point(data=data,aes(x=day-7,y=cumulative))
 
-gg
-# res2 <- res %>%
-#   group_by(week) %>%
-#   summarise(mean=mean(cumulative))
+sum(detect_extinct(daily_results,params$cap_cases)$extinct)
 
-# Plot of weekly cases
-# ggplot2::ggplot(data=res, ggplot2::aes(x=week, y=cumulative, col = as.factor(sim))) +
-#   ggplot2::geom_line(show.legend = FALSE, alpha=0.3) +
-#   ggplot2::scale_y_continuous(name="Number of cases") +
-#   ggplot2::theme_bw()
-#
-# ringbp::extinct_prob(res,cap_cases = 4500)
+# output_sims <- c(42,8,12)
+# branching_output(results,output_sims,params)
 
-# plot(res2$week,res2$mean)
